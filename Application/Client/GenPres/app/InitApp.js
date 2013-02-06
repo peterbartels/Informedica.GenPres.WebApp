f2e1494194c2f6ad4948f9f4d0f54c42e32770d0
Peter Bartels <peterbartels@gmail.com>
2013-02-06 12:55:19 +0000

    Got the loginform working with extjs now

--- Application/Client/GenPres/app/InitApp.js
+++ Application/Client/GenPres/app/InitApp.js
@@ -7,7 +7,11 @@
  */
 Ext.Loader.setConfig({
     enabled: true,
-    disableCaching: true
+    disableCaching: true,
+    paths : {
+        Ext: '.',
+        GenPres : './Client/GenPres/app/'
+    }
 });
 
 Ext.require([
@@ -17,6 +21,13 @@ Ext.require([
 Ext.onReady(function () {
     Ext.direct.Manager.addProvider(Ext.app.REMOTING_API);
 
-    Ext.app.config.appFolder = './Client/GenPres/app';
-    Ext.application(Ext.app.config);
+    //Ext.app.config.appFolder = './Client/GenPres/app';
+    Ext.application({
+        requires : 'GenPres.view.user.LoginWindow',
+        name: 'test',
+        launch: function() {
+            var loginWindow = Ext.create('GenPres.view.user.LoginWindow');
+            loginWindow.show();
+        }
+    });
 });
