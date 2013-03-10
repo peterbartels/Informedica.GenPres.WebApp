8d49813d79153e4811e36d6dfe606527d0c01ef4
Peter Bartels <peterbartels@gmail.com>
2013-03-10 15:04:51 +0000

    Added Management for users, logical units and patients

---
+++ Application/Controllers/ManagementController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ext.Direct.Mvc;
+
+namespace Informedica.GenPres.WebApp.Controllers
+{
+    public class ManagementController : DirectController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetUsers()
+        {
+            return Json(new ArrayList()
+            {
+                new
+                    {
+                        username = "peter",
+                        password = "test",
+                        id = "test"
+                    }
+            });
+        }
+
+        public ActionResult SaveUser(UserDto user)
+        {
+            return Json(new {success = user.username});
+        }
+    }
+
+    public class UserDto
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
